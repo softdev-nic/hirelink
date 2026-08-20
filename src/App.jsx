d8e94705cab9e45e049bc34bd5d9b0f6aef98a28
softdev-nic <softtechon.inc@gmail.com>
2026-08-20 12:31:37 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
- import React from 'react'
- import Registration from './pages/reg/Registration'
- import Login from './pages/reg/Login'
- import ForgotPassword from './pages/reg/ForgotPassword'
- import ResetPassword from './pages/reg/ReserPassword'
- import Home from './pages/Home'
- import {Navigate,Route, Routes} from "react-router-dom"
+ import React from "react";
+import Registration from "./pages/reg/Registration";
+import Login from "./pages/reg/Login";
+import ForgotPassword from "./pages/reg/ForgotPassword";
+import ResetPassword from "./pages/reg/ReserPassword";
+import Home from "./pages/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [IsLoggedIn,setIsLoggedIn] = React.useState(false)
-  const getLoggenIn = ()=>{
-    setIsLoggedIn(localStorage.getItem("loggenIn"))
-  }
-  React.useEffect(()=>{
-    getLoggenIn
-  },[])
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
+  const getLoggedIn = () => {
+    const loggedIn = localStorage.getItem("loggenIn");
+    setIsLoggedIn(loggedIn === "true");
+  };
+
+  React.useEffect(() => {
+    getLoggedIn();
+  }, []);
+
   return (
     <Routes>
-      <Route path="/" element={IsLoggedIn? <Navigate to="/home" replace/> : <Navigate to="/login" replace />} />
+      <Route
+        path="/"
+        element={
+          isLoggedIn
+            ? <Navigate to="/home" replace />
+            : <Navigate to="/login" replace />
+        }
+      />
+
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Registration />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
-      {/* <Route path="*" element={} /> */}
       <Route path="/reset-password/:token" element={<ResetPassword />} />
       <Route path="/home" element={<Home />} />
     </Routes>
